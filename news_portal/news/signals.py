9347f90c0d32b40c4ef7aa95feed46bd1ccf6bc5
RUBIN115 <120738934+RUBIN115@users.noreply.github.com>
2023-05-04 16:40:45 +1100

    Модуль D.9.

--- news_portal/news/signals.py
+++ news_portal/news/signals.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 def send_notifications(preview, pk, title, subscribers):
     html_content = render_to_string(
-        'post_created_email.html',
+        'sent_email.html',
         {
             'text': preview,
             'link': f'{settings.SITE_URL}/news/{pk}',
