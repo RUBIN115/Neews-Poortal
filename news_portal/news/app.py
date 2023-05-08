4411be487cb9ee47ad95e3f22719d95f7b8ed3c3
RUBIN115 <120738934+RUBIN115@users.noreply.github.com>
2023-05-08 19:54:29 +1100

    Модуль D.9.

---
+++ news_portal/news/app.py
@@ -0,0 +1,8 @@
+import redis
+
+
+red = redis.Redis(
+    host='redis-14442.c1.asia-northeast1-1.gce.cloud.redislabs.com',
+    port=14442,
+    password='e545c1qZwnEeK4nstTFhR1hmOhnfICvj'
+)
--- news_portal/news_portal/settings.py
+++ news_portal/news_portal/settings.py
@@ -186,4 +186,8 @@ SERVER_EMAIL = 'ilhaosinkin@yandex.ru'  # это будет у нас вмест
 
 ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 3
 
-
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
