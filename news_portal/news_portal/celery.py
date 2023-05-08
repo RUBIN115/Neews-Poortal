650062a56e246e34fd76612918197f39ac1da29a
RUBIN115 <120738934+RUBIN115@users.noreply.github.com>
2023-05-08 20:50:14 +1100

    Модуль D.10.

--- news_portal/news_portal/celery.py
+++ news_portal/news_portal/celery.py
@@ -1,9 +1,9 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news_portal.settings')
 
-app = Celery('news')
+app = Celery('news_portal')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
